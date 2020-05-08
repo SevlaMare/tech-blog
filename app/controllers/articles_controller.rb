5f5d038a1fe7b945f0a6142ec89fa799c7cf021d
SevlaMare <sevilamare@gmail.com>
2020-05-08 18:40:40 -0300

    flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' Created!"
     redirect_to article_path(@article)
   end
 
@@ -25,6 +26,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    flash.notice = "Article '#{@article.title}' Deleted!"
     redirect_to articles_path(@articles)
   end
 
@@ -36,6 +38,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Edited!"
     redirect_to article_path(@article)
   end
 end
