ae492c9b349cd4968594af1abf19c64b30686380
SevlaMare <sevilamare@gmail.com>
2020-05-10 15:45:31 -0300

    precompile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_004723) do
+ActiveRecord::Schema.define(version: 2020_05_07_004732) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,15 +25,15 @@ ActiveRecord::Schema.define(version: 2020_05_07_004723) do
   create_table "comments", force: :cascade do |t|
     t.string "author_name"
     t.text "body"
-    t.integer "article_id"
+    t.bigint "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "taggings", force: :cascade do |t|
-    t.integer "tag_id"
-    t.integer "article_id"
+    t.bigint "tag_id"
+    t.bigint "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_taggings_on_article_id"
@@ -43,4 +46,7 @@ ActiveRecord::Schema.define(version: 2020_05_07_004723) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
+  add_foreign_key "taggings", "articles"
+  add_foreign_key "taggings", "tags"
 end
